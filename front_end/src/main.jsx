a5a1376eb93e6f3ea3e0b4ab7583cd235039143f
Pranav Chaudhary <Pranav.Chaudhary@Geminisolutions.com>
2026-06-13 22:57:15 +0530

    Changing ServerURL

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -3,16 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import UserContext from './context/userContext.jsx'
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
   <BrowserRouter>
-  <UserContext>
     <App />
-    </UserContext>
     </BrowserRouter>
     </Provider>
   ,
